d4f9a06c57db14812f40dc3dc29349e4085bb9c4
Aden T. <106639051+aden-tt@users.noreply.github.com>
2026-09-20 17:42:56 -0400

    Create a camera on the stage

--- usd-thumbnail-generator.py
+++ usd-thumbnail-generator.py
@@ -8,13 +8,14 @@ Author: Aden Thompson
 
 
 to-do:
- - ask what path to output thumbnails and previews
+ - ask ASFW what path to output thumbnails and previews
+ - create camera and properly frame the asset
  - convert from hardcoded paths to user input
  - take multiple usda args
 
 """
 
-from pxr import Usd, UsdMedia
+from pxr import Usd, UsdMedia, Sdf, UsdGeom
 import subprocess
 import posixpath
 
@@ -44,11 +45,12 @@ if not prim:
 
 
 # Render a thumbnail using usdrecord
-def render_thumbnail(usd_path, output_png_path):
+def render_thumbnail(usd_path: str, output_png_path: str, camera_p: str="/World/MyPerspCam"):
     cmd = [
         "cmd.exe",
         "/c",
         USDRECORD,
+        "--camera", camera_p,
         usd_path,
         output_png_path
     ]
@@ -73,18 +75,48 @@ def update_usd_thumbnail():
 
     stage.GetRootLayer().Save()
 
+#
+def create_perspective_camera(stage: Usd.Stage, prim_path: str="/World/MyPerspCam") -> UsdGeom.Camera:
+    camera_path = Sdf.Path(prim_path)
+    usd_camera: UsdGeom.Camera = UsdGeom.Camera.Define(stage, camera_path)
+    usd_camera.CreateProjectionAttr().Set(UsdGeom.Tokens.perspective)
+    return usd_camera
+
+
+
 
 # ---------------------------------------------------------------------------
 # MAIN
 # ---------------------------------------------------------------------------
 
-
 def main():
+
+
+    default_prim = UsdGeom.Xform.Define(stage, Sdf.Path("/World"))
+    stage.SetDefaultPrim(default_prim.GetPrim())
+
+    # Create the perspective camera at /World/MyPerspCam
+    cam_path = default_prim.GetPath().AppendPath("MyPerspCam")
+    camera = create_perspective_camera(stage, cam_path)
+
+    # Export the complete Stage as a string and print it.
+    usda = stage.GetRootLayer().ExportToString()
+    print(usda)
+
+    # Check that the camera was created
+    prim = camera.GetPrim()
+    assert prim.IsValid()
+    assert camera.GetPath() == Sdf.Path(cam_path)
+    assert prim.GetTypeName() == "Camera"
+    projection = camera.GetProjectionAttr().Get()
+    assert projection == UsdGeom.Tokens.perspective
+
     # alab_full = r"D:\ALab\ALab-2.3.0\ALab\entry.usda"
     render_thumbnail(USDA_PATH, OUTPUT_PATH) # test render book
     print(f"Rendered thumbnail: {OUTPUT_PATH}")
+
+    stage.RemovePrim(camera.GetPath())
     update_usd_thumbnail()
-    pass
 
 
 if __name__ == "__main__":
